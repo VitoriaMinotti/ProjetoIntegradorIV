92c269f0fb6db1cacf85b5e267c89e623c30787f
VitoriaMinotti <vitorialminotti@hotmail.com>
2023-04-08 15:05:35 -0300

    Criação da tela de splash

---
+++ app/src/main/java/com/example/projetointegrador/Splash.java
@@ -0,0 +1,41 @@
+package com.example.projetointegrador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    private final Timer timer = new Timer();
+    TimerTask timerTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        gotoMainActivity();
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 3000);
+    }
+
+    private void gotoMainActivity() {
+        Intent  intent =  new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
